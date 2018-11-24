44b853282c57cda6ecc2046f978dc86d199a7271
Brenton Annan <brentonannan@users.noreply.github.com>
2018-11-24 17:47:48 +1100

    [WIP] testing a button of the game page
    Co-authored-by: Michael Milewski <saramic@gmail.com>
    Co-authored-by: Selena Small <selenawiththetattoo@gmail.com>

--- spec/features/games_spec.rb
+++ spec/features/games_spec.rb
@@ -4,6 +4,9 @@ RSpec.feature "Games", type: :feature, js: true do
   scenario 'Visiting the game' do
     visit '/'
     expect(page).to have_content 'Welcome to the Game'
+
+    pending "there is a fire in the building and we have to leave."
+    page.find('button').click
   end
 end
 
