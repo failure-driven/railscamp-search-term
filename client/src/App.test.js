ea441c9dfecf4ce2e96d5534773642ce9e16e683
Emily Coats <gamzatti@users.noreply.github.com>
2018-11-24 17:19:12 +1100

    Add welcome message
    Co-authored-by: Selena Small <selenawiththetattoo@gmail.com>
    Co-authored-by: Michael Milewski <saramic@gmail.com>

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
+import {shallowToJson} from 'enzyme-to-json';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Welcome to the Game</h2>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+  expect(shallowToJson(wrapper)).toMatchSnapshot();
 });
