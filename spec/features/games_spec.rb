af7cd6043389f6810afc462df5cee807df39dc92
Paul Douglas <pdouglasssi@users.noreply.github.com>
2018-11-24 18:26:22 +1100

    Test for game rounds - pended as no code for rounds yet.
    Co-authored-by: Michael Milewski <saramic@gmail.com>
    Co-authored-by: Selena Small <selenawiththetattoo@gmail.com>

--- spec/features/games_spec.rb
+++ spec/features/games_spec.rb
@@ -6,6 +6,8 @@ RSpec.feature "Games", type: :feature, js: true do
     expect(page).to have_content 'Welcome to the Game'
 
     page.find('button').click
+    pending("Haven't written any rounds yet")
+    expect(page).to have_content 'This is round 1'
   end
 end
 
