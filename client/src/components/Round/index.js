d0a45a60a89aa3d9175185c8dda686d02cd577df
Chris Aitchison <cmaitchison@users.noreply.github.com>
2018-11-25 12:13:49 +1100

    Refactor round.js to be a class
    Co-authored-by: Michael Milewski <saramic@gmail.com>
    Co-authoured-by: Selena Small <selenawiththetattoo@gmail.com>

--- client/src/components/Round/index.js
+++ client/src/components/Round/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default () => (
-  <>
-    <div className='status'></div>
-    <h2>This is round 1</h2>
-    <img src="http://localhost:5000/8e85e82e854.gif"/>
-    <textarea>
-    </textarea>
-  </>
-)
+class Round extends Component {
+
+  render() {
+    return(
+      <>
+      <div className='status'></div>
+      <h2>This is round 1</h2>
+      <img src="http://localhost:5000/8e85e82e854.gif"/>
+      <textarea>
+      </textarea>
+    </>
+    );
+  }
+}
+
+export default Round;
